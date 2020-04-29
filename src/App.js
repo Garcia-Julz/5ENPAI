7be6865795c91b33d208ccb542b9059e50746883
Julian Garcia <garcia.j.julian@gmail.com>
2020-04-29 10:43:28 -0400

    added edits to the nav as well as the link under the splashpage

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import React from 'react';
-import background from './assets/5enpai_background_service_homepage.png';
+import Logo from './assets/5ENPAI_Logo_home_.png';
 import './App.css';
+import './JoinButton.scss';
+// import Button from './JoinButton';
 
 function App() {
   return (
     <>
     <div className="bgimg w3-display-container w3-animate-opacity w3-text-white">
-      <div className="w3-display-topleft w3-padding-large w3-xlarge">
-        Logo
+      <div className="logo container">
+        <div className="w3-display-topleft w3-padding-large w3-xlarge" style={{"display" : "flex"}}>
+          5ENPAI
+          <img src={Logo} style={{"width" : "26%", "margin-left" : "3%"}}/>
+        </div>
+        <div className="w3-display-topright w3-padding-large w3-xlarge">
+          <a href="faq">
+            FAQs
+          </a>
+        </div>
       </div>
       <div className="w3-display-middle">
-        <h1 className="w3-jumbo w3-animate-top">COMING SOON</h1>
+        <h1 className="w3-xxxlarge w3-animate-top">RETHINK BUYING</h1>
         <hr id="border1" className="w3-border-grey" />
-        <p className="w3-large w3-center">35 days left</p>
+        <p className="w3-large w3-center">Join our Beta Launch List 🚀</p>
+        <div className="join-link">
+          <a className="brk-btn" href="#" class="brk-btn">
+            Join Now
+          </a>
+        </div>
       </div>
       <div className="w3-display-bottomleft w3-padding-large">
-        Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a>
+        Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3</a>
       </div>
     </div>
     </>
---
+++ src/JoinButton.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+class Button extends Component {
+  render() {
+    return <button color="red" />;
+  }
+}
+export default Button;
